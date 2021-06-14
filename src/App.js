e08dbb863765b54af8589e59abdcfc1c6411b31d
Alexander Gonzalo Urbina Mar <43892122+Ariandel007@users.noreply.github.com>
2021-06-13 20:28:51 -0500

    menu modify

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import './App.scss';
 import Home from './_components/Home';
 import SignUp from './_components/SignUp';
@@ -11,17 +11,32 @@ import store from './store';
 
 
 function App() {
+  const sideNavRef = React.useRef();
+
+  const openSideNav = () => sideNavRef.current.style.width = "250px";
+  const closeSideNav = () => sideNavRef.current.style.width = "0px";
+
   return (
     <BrowserRouter>
     <Provider 
       store={store}
     >
+      <div className="col-2 selectable"><h2 onClick={openSideNav} className="header-title">☰</h2></div>
+
       <div className="container">
+      
         <Switch>
           <Route exact path="/" component={Home}></Route>
           <Route exact path="/sign-up" component={SignUp}></Route>
           <Route exact path="/receipt" component={Receipt}></Route>
         </Switch>
+
+        <div id="mySidenav" className="sidenav" ref={sideNavRef}>
+          <h2 onClick={closeSideNav} className="ml-3 selectable header-title">☰</h2>
+          <Link className="header-title my-2 colornav" to={'/receipt'} onClick={closeSideNav}>Inicio</Link>
+          <Link className="header-title my-2 colornav" to={'/historial'} onClick={closeSideNav}>Historial</Link>
+          <Link className="header-title my-2 colornav" to={'/'}  onClick={closeSideNav}>Cerrar Sesión</Link>
+        </div>
       </div>
 
       </Provider>
