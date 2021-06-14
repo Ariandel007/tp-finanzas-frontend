190ca5755b18144cb714bccbc3dd8d6ccb8bccfb
Alexander Gonzalo Urbina Mar <43892122+Ariandel007@users.noreply.github.com>
2021-06-13 21:44:48 -0500

    save actions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
 import authReducer from './auth-reducer';
-
+import receiptReducer from './receipt-reducer';
 
 // de este modo podremos tener varios reducers a futuro
 export default combineReducers({
     auth: authReducer,
+    receipt: receiptReducer
 });
---
+++ src/reducers/receipt-reducer.js
@@ -0,0 +1,50 @@
+import {
+    SAVE_RECEIPT,
+    SAVE_RECEIPT_SUCCESS,
+    SAVE_RECEIPT_ERROR,
+    LIST_RECEIPTS,
+    LIST_RECEIPTS_SUCCESS,
+    LIST_RECEIPTS_ERROR
+
+} from '../types';
+
+const initialState = {
+    receipt: null,
+    loading: false,
+    receiptList: []
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default function(state = initialState, action) {
+    switch (action.type) {
+        case SAVE_RECEIPT:
+        case LIST_RECEIPTS:
+            return {
+                ...state,
+                loading: true,
+            }
+
+        case SAVE_RECEIPT_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                receipt: action.payload.receipt,
+            }
+        
+        case SAVE_RECEIPT_ERROR:
+        case LIST_RECEIPTS_ERROR:
+            return {
+                ...state,
+                loading: false
+            }
+        case LIST_RECEIPTS_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                receiptList: action.payload.receiptList
+            }
+
+        default:
+            return state;
+    }
+}
